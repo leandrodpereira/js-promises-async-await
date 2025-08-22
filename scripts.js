2f7c634f5ade32cb9c5ea83c5098941ef1bcd95c
Leandro <leandro.pereira@fepi.br>
2025-08-22 16:47:19 -0300

    feat: validacao js tipo e tamanho da imagem

--- scripts.js
+++ scripts.js
@@ -4,3 +4,16 @@ const inputUpload = document.getElementById('image-upload');
 uploadBtn.addEventListener('click', () => {
     inputUpload.click();
 });
+
+inputUpload.addEventListener('change', (event) => {
+    const file = event.target.files[0];
+    if (!file.type.match('image.*')) {
+        alert('Por favor, selecione uma imagem PNG ou JPEG.');
+        return;
+    }
+
+    if (file.size > 2 * 1024 * 1024) { // 2MB
+        alert('O arquivo é muito grande. Por favor, selecione uma imagem menor que 2MB.');
+        return;
+    }
+});
