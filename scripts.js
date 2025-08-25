b044754f9d34f8f6ccb5281f3d15d3812e5f95fe
leandrodpereira <leandrodpereira@gmail.com>
2025-08-25 16:19:30 -0300

    feat: funções assíncronas

--- scripts.js
+++ scripts.js
@@ -5,15 +5,50 @@ uploadBtn.addEventListener('click', () => {
     inputUpload.click();
 });
 
-inputUpload.addEventListener('change', (event) => {
-    const file = event.target.files[0];
-    if (!file.type.match('image.*')) {
+
+function arquivoValido(arquivo) {
+    if (!arquivo.type.match('image.*')) {
         alert('Por favor, selecione uma imagem PNG ou JPEG.');
-        return;
+        return false;
     }
 
-    if (file.size > 2 * 1024 * 1024) { // 2MB
+    if (arquivo.size > 2 * 1024 * 1024) { // 2MB
         alert('O arquivo é muito grande. Por favor, selecione uma imagem menor que 2MB.');
-        return;
+        return false;
     }
+
+    return true;
+}
+
+
+const imagemPrincial = document.querySelector(".main-imagem");
+const nomeDaImagem = document.querySelector(".container-imagem-nome p");
+
+function lerConteudoDoArquivo(arquivo) {
+    return new Promise((resolve, reject) => {
+        const leitor = new FileReader();
+        leitor.onload = () => {
+            resolve({url: leitor.result, nome: arquivo.name});
+        };
+
+        leitor.onerror = () => {
+            reject(`Erro ao ler o arquivo ${arquivo.name}`);
+        };
+        
+        leitor.readAsDataURL(arquivo);
+    });
+}
+
+inputUpload.addEventListener('change', async (event) => {
+    const arquivo = event.target.files[0];
+    if (arquivoValido(arquivo)) {
+        try {
+            const { url, nome } = await lerConteudoDoArquivo(arquivo);
+            imagemPrincial.src = url;
+            nomeDaImagem.textContent = nome;
+        } catch (error) {
+            console.log("Erro ao ler o arquivo");
+        }
+    }   
+    
 });
